4d095f800215173ea8d924901a690e2927175453
rafael.cruz <rafaelcruz.net81@gmail.com>
2024-02-23 08:18:12 -0300

    Adicionando pagina de listar cliente

--- src/main/java/br/com/ibmec/backend/cadastrocliente/controller/ClienteController.java
+++ src/main/java/br/com/ibmec/backend/cadastrocliente/controller/ClienteController.java
@@ -1,13 +1,18 @@
 package br.com.ibmec.backend.cadastrocliente.controller;
 
 import br.com.ibmec.backend.cadastrocliente.model.Cliente;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@Controller
+@RequestMapping("/cliente")
 public class ClienteController {
 
-    public void usaCliente() {
-        Cliente cliente  = new Cliente();
-        cliente.setNome("Rafael Cruz");
-
-        String nome = cliente.getNome();
+    @GetMapping("/listar")
+    public String listarCliente() {
+        return "listar-cliente";
     }
+
 }
